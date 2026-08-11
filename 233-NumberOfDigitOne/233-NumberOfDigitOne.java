46a7a46b641720bfe5c1e8e5c85ea553a5e9703c
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-08-11 14:20:45 +0530

    Create 233-NumberOfDigitOne

---
+++ 233-NumberOfDigitOne/233-NumberOfDigitOne.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 2:20:44 PM
+class Solution
+{
+public int countDigitOne(int n) {
+
+    if (n <= 0) return 0;
+    int q = n, x = 1, ans = 0;
+    do {
+        int digit = q % 10;
+        q /= 10;
+        ans += q * x;
+        if (digit == 1) ans += n % x + 1;
+        if (digit >  1) ans += x;
+        x *= 10;
+    } while (q > 0);
+    return ans;
+}
+}
