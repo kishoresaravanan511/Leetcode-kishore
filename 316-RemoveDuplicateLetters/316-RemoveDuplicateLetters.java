eb37aadef957f49b65a280a326c1c7a3a10d4e56
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-08-11 14:19:50 +0530

    Create 316-RemoveDuplicateLetters

---
+++ 316-RemoveDuplicateLetters/316-RemoveDuplicateLetters.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 2:19:48 PM
+public class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] cnt = new int[26];
+        int pos = 0;
+        for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < s.charAt(pos)) pos = i;
+            if (--cnt[s.charAt(i) - 'a'] == 0) break;
+        }
+        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
+    }
+}
