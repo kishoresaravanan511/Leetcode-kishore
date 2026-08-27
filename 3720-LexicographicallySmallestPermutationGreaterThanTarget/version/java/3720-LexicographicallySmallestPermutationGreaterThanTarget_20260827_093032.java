9d49c4d334580f0802070248dff82c7cb0fdba4f
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-08-27 09:30:33 +0530

    Create 3720-LexicographicallySmallestPermutationGreaterThanTarget

---
+++ 3720-LexicographicallySmallestPermutationGreaterThanTarget/version/java/3720-LexicographicallySmallestPermutationGreaterThanTarget_20260827_093032.java
@@ -0,0 +1,22 @@
+// Last updated: 8/27/2026, 9:30:32 AM
+1class Solution {
+2    public int[] arrayRankTransform(int[] arr) {
+3        int n = arr.length;
+4        int[] needToBeSorted = arr.clone();
+5        Arrays.sort(needToBeSorted);
+6        int ind = 1;
+7        Map<Integer,Integer> m = new HashMap<>();
+8
+9        for(int x : needToBeSorted)
+10        {
+11            if(!m.containsKey(x))
+12                m.put(x,ind++);
+13        }
+14        int[] res = new int[arr.length];
+15        for(int i=0;i<n;i++)
+16        {
+17            res[i] = m.get(arr[i]);
+18        }
+19        return res;
+20    }
+21}
