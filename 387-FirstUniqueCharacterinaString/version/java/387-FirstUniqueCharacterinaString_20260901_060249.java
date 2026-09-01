b99ca3d2097b6072e6cc9b09d5263a794a8ef836
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-09-01 06:02:50 +0530

    Create 387-FirstUniqueCharacterinaString

---
+++ 387-FirstUniqueCharacterinaString/version/java/387-FirstUniqueCharacterinaString_20260901_060249.java
@@ -0,0 +1,20 @@
+// Last updated: 9/1/2026, 6:02:49 AM
+1class Solution {
+2    public int firstUniqChar(String s) {
+3        int[] freq = new int[256];
+4        for(int i=0;i<s.length();i++)
+5        {
+6            char ch = s.charAt(i);
+7            freq[ch]++;
+8        }
+9    
+10        for(int i=0;i<s.length();i++)
+11        { 
+12            //important, we check the frequency with the order of 's' not index based.  that's why array size is 256 for unicode characters.
+13            char ch = s.charAt(i);
+14            if(freq[ch]==1)   //first occured..
+15                return i;
+16        }
+17        return -1;
+18    }
+19}
