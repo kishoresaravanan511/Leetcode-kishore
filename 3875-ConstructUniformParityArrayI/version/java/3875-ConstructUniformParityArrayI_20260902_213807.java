ebf6155fc1e8c7b95da6624fc42c513207f3a3bc
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-09-02 21:38:07 +0530

    Create 3875-ConstructUniformParityArrayI

---
+++ 3875-ConstructUniformParityArrayI/version/java/3875-ConstructUniformParityArrayI_20260902_213807.java
@@ -0,0 +1,16 @@
+// Last updated: 9/2/2026, 9:38:07 PM
+1class Solution {
+2    public boolean uniformArray(int[] nums1) {
+3        // int n = nums1.length;
+4        // List<Integer> l = new ArrayList<>();
+5
+6        // for(int i=0;i<n;i++)
+7        // {
+8        //     for(int j=0;j<n;j++)
+9        //     {
+10
+11        //     }
+12        // }
+13        return true;
+14    }
+15}
