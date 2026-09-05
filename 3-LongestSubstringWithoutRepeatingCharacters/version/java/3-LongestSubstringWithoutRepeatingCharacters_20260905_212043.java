23c4fa84c61da3fc86dc4e8106f1d1fa1f28f7db
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-09-05 21:20:44 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/java/3-LongestSubstringWithoutRepeatingCharacters_20260905_212043.java
@@ -0,0 +1,24 @@
+// Last updated: 9/5/2026, 9:20:43 PM
+1class Solution{
+2public int reverse(int x)
+3{
+4    int result = 0;
+5
+6    while (x != 0)
+7    {
+8        int tail = x % 10;
+9        int newResult = result * 10 + tail;
+10
+11        //edge case
+12        if ((newResult - tail) / 10 != result)
+13        {
+14            return 0; 
+15        }
+16
+17        result = newResult;
+18        x = x / 10;
+19    }
+20
+21    return result;
+22}
+23}
