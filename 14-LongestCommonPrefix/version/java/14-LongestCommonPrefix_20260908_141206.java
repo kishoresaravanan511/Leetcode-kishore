036d1acccd96a0cd3358f41b96eff9c9bbf19b5f
KishoreSaravanan <kishoresaravanan511@gmail.com>
2026-09-08 14:12:07 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/version/java/14-LongestCommonPrefix_20260908_141206.java
@@ -0,0 +1,22 @@
+// Last updated: 9/8/2026, 2:12:06 PM
+1class Solution {
+2    public String longestCommonPrefix(String[] strs) {
+3        String res = "";
+4        Arrays.sort(strs);
+5        String fi = strs[0];
+6        String lt = strs[strs.length-1];
+7
+8        for(int i=0;i<fi.length();i++)
+9        {
+10            if(fi.charAt(i) == lt.charAt(i))
+11            {
+12                res+=fi.charAt(i);
+13            }
+14            else
+15            {
+16                break;
+17            }
+18        }
+19        return res;
+20    }
+21}
